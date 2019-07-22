2777ebc68c16089ff83cd44183aa703606b03460
Trang Nguyen <ttn2127@columbia.edu>
2019-07-22 15:33:13 -0400

    added route to home

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -36,19 +36,6 @@ function MovieDetails({ movie }) {
   return (
     <div className="movie-card">
       <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
     </div>
   );
 }
