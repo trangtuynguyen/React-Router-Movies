b79125bc42433fd95ef12fe15d90cd4ee6c465ae
Trang Nguyen <ttn2127@columbia.edu>
2019-07-22 15:28:19 -0400

    added route

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
+
   const [movie, setMovie] = useState();
+  
  
   useEffect(() => {
-    const id = 1;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
+    const id = props.match.params.id;
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
+          console.log(id);
           setMovie(response.data);
         })
         .catch(error => {
